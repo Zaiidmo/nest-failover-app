c89704f0b58c7425fd2c533b1c5db14fbaf7b977
Zaiidmo <Zaiidmoumnii@gmail.com>
2025-01-08 10:03:02 +0100

    Installing NestConfig and Configuring The Databases Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async () => ({
+        uri: process.env.PRIMARY_DATABASE, // Primary Database URI
+        connectionName: 'primary', //Connection Alias
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async () => ({
+        uri: process.env.SECONDARY_DATABASE, // Secondary Database URI
+        connectionName: 'secondary',// Connection Alias
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
